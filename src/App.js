ea0e57a1ab3f07cb5a58f3185b5079150632a4ea
Olga <oolya.kozhevnikova@gmail.com>
2018-08-26 13:02:04 +0530

    Display books on shelves

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import './App.css'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import FindBook from './FindBook'
+import * as BooksAPI from './BooksAPI'
+import escapeRegExp from 'escape-string-regexp'
 
 
 class App extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+		currentlyReading: [],
+		wantToRead: [],
+		read: []
   }
+
+  componentDidMount() {
+		this.getBooks()
+  }
+
+  getBooks() {
+		BooksAPI.getAll().then(books => {
+			// escapeRegExp means: if there are any special characters (backslash, etc) inside the query,
+      // then go ahead and escape them,
+      // so we use those special characters as a string literal
+      // rather than these special regexp characters
+			const matchCurrentlyReading = new RegExp(escapeRegExp('currentlyReading'));
+			let currentlyReading = books ? books.filter(book => matchCurrentlyReading.test(book.shelf)) : null;
+
+			const matchWantToRead = new RegExp(escapeRegExp('wantToRead'));
+			let wantToRead = books ? books.filter(book => matchWantToRead.test(book.shelf)) : null;
+
+			const matchRead = new RegExp(escapeRegExp('read'));
+			let read = books ? books.filter(book => matchRead.test(book.shelf)) : null;
+
+			this.setState({ currentlyReading, wantToRead, read });
+		})
+	}
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks/>
+          <ListBooks
+            currentlyReading={this.state.currentlyReading}
+            wantToRead={this.state.wantToRead}
+            read={this.state.read}
+          />
         )}/>
         <Route path="/search" render={() => (
           <FindBook/>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,51 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
-import * as BooksAPI from './BooksAPI'
-import escapeRegExp from 'escape-string-regexp'
+
 
 class ListBooks extends Component {
-	state = {
-		currentlyReading: [],
-		wantToRead: [],
-		read: []
-	}
-
-	componentDidMount() {
-		this.getBooks()
-	}
-
-	getBooks() {
-		BooksAPI.getAll().then(books => {
-			// escapeRegExp means: if there are any special characters (backslash, etc) inside the query,
-      // then go ahead and escape them,
-      // so we use those special characters as a string literal
-      // rather than these special regexp characters
-			const matchCurrentlyReading = new RegExp(escapeRegExp('currentlyReading'));
-			let currentlyReading = books ? books.filter(book => matchCurrentlyReading.test(book.shelf)) : null;
-
-			const matchWantToRead = new RegExp(escapeRegExp('wantToRead'));
-			let wantToRead = books ? books.filter(book => matchWantToRead.test(book.shelf)) : null;
-
-			const matchRead = new RegExp(escapeRegExp('read'));
-			let read = books ? books.filter(book => matchRead.test(book.shelf)) : null;
-
-			this.setState({ currentlyReading, wantToRead, read });
-			console.log(this.state)
-		})
-	}
-
 	showShelf(books, shelfTitle) {
 		return (
 			<div className="bookshelf">
 				<h2 className="bookshelf-title">{shelfTitle}</h2>
 				<div className="bookshelf-books">
 					<ol className="books-grid">
-						<li>
-							{books.map(book => (
+						{books.map(book => (
+							<li key={book.id}>
 								<Book book={book}/>
-							))}
-						</li>
+							</li>
+						))}
 					</ol>
 				</div>
 			</div>
@@ -53,8 +22,8 @@ class ListBooks extends Component {
 	}
 
   render() {
+		console.log(this.state)
 		const { currentlyReading, wantToRead, read } = this.props
-		console.log(currentlyReading)
 		return (
 			<div className="list-books">
 				<div className="list-books-title">
@@ -62,9 +31,9 @@ class ListBooks extends Component {
 				</div>
 				<div className="list-books-content">
 					<div>
-						{/* {this.showShelf(currentlyReading, 'Currently Reading')}
+						{this.showShelf(currentlyReading, 'Currently Reading')}
 						{this.showShelf(wantToRead, 'Want to read')}
-						{this.showShelf(read, 'Read')} */}
+						{this.showShelf(read, 'Read')}
 					</div>
 				</div>
 				<div className="open-search">
